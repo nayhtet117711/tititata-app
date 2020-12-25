0256931acc921ff4ecf006c5f7da80dfe61e9f93
nayhtet <nayhtet117711@gmail.com>
2020-12-25 13:01:28 +0630

    updates

--- web-server/src/config/api.js
+++ web-server/src/config/api.js
@@ -1,8 +1,8 @@
 
-export const BASE_URL = "http://192.168.100.59:10101"
+// export const BASE_URL = "http://192.168.100.59:10101"
 
-export const API = "http://192.168.100.59:10101"
+// export const API = "http://192.168.100.59:10101"
 
-// export const BASE_URL = "https://tititata.online"
+export const BASE_URL = "https://tititata.online"
 
-// export const API = "https://tititata.online/api"
+export const API = "https://tititata.online/api"
