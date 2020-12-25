d175a665b3d7f336d935fe851373c291ce513fe6
nayhtet <nayhtet117711@gmail.com>
2020-12-25 14:58:41 +0630

    added

--- web-server/src/components/testVideoCall2.js
+++ web-server/src/components/testVideoCall2.js
@@ -19,14 +19,47 @@ class TestVideoCall {
         this.videoContainer = document.getElementById("video-container")
         this.localVideo = document.getElementById("local-video")
         this.remoteVideo = document.getElementById("remote-video")    
+        this.cameraAllowed = false
+        this.micAllowed = false
+        this.incomingRing = new window.Audio("https://proxy.notificationsounds.com/wake-up-tones/door-knock-478/download/file-69_door-knock-2.mp3");
+        this.incomingRing.volume = 0.5;
+        this.incomingRing.loop = true;
+        this.outgoingRing = new window.Audio("https://proxy.notificationsounds.com/wake-up-tones/good-morning-502/download/file-sounds-727-good-morning.mp3")
+        this.outgoingRing.volume = 0.5;
+        this.outgoingRing.loop = true
+
+        
+    }
+
+    checkPermission = (permissionName, callback) => {
+        try {
+        navigator.permissions.query(Object.assign({name: permissionName}))
+        .then(function (permission) {
+           if(permission.state==="denied") {
+                callback(false)
+           } else callback(true)
+        });
+        } catch (e) {
+        }
     }
 
     getUserMedia = (constraints) => {
+        console.log(navigator.mediaDevices)
         return navigator.mediaDevices.getUserMedia(constraints);
     }
 
     listen = async () => {
-        console.log("listen")
+
+        await this.getUserMedia({ video: true, audio: true })
+
+        setTimeout(() => {
+            this.checkPermission("camera", permit=> {
+                this.cameraAllowed = permit
+            })
+            this.checkPermission("microphone", permit=> {
+                this.micAllowed = permit
+            })
+        }, 10)
         
         this.socket.on("make-call", this.onMakeCall)  // Call Receiver
 
@@ -42,40 +75,57 @@ class TestVideoCall {
     }
 
     makeCall = async (socketId) => {
-        console.log("makeCall socketId: ", socketId)
-        this.roomId = this.socket.socketId
-        this.toSocketIds = [socketId]
-        await this.initLocalStream()
-        this.socket.emit("make-call", { roomId: this.roomId, toSocketIds: [socketId] })
-        this.videoContainer.style.display = "block"
+        this.outgoingRing.play()
+        setTimeout(async() => {
+            if(!this.cameraAllowed || !this.micAllowed) {
+                this.outgoingRing.pause()
+                return alert("You can't call! Please allow camera and microphone permissions!")
+            }
+            this.roomId = this.socket.socketId
+            this.toSocketIds = [socketId]
+            await this.initLocalStream()
+            this.socket.emit("make-call", { roomId: this.roomId, toSocketIds: [socketId] })
+            this.videoContainer.style.display = "block"
 
-        // setTimeout(() => {
-        //     if(this.peerConnection===null) {
-        //         this.onStateChange(null, true)
-        //     }
-        // }, 10000)
+            setTimeout(() => {
+                if(this.peerConnection===null) {
+                    this.outgoingRing.pause()
+                    this.onStateChange(null, true)
+                }
+            }, 20000)
+        }, 10)
     }
 
     onMakeCall = async (data) => { // Call Receiver
-        console.log("call receive")
-        const confirmResult = window.confirm("Incomming call from "+ data.roomId)
-        if(confirmResult) {
-            const { roomId } = data
-            await this.initLocalStream()
-            this.roomId = roomId
-            this.peerConnection = new RTCPeerConnection(config)
-            this.addLocalTrack()
-            this.peerConnection.ontrack = this.initRemoteStream
-            this.peerConnection.onicecandidate = this.sendIceCandidate
-            this.peerConnection.oniceconnectionstatechange = this.onStateChange
-            await this.createOffer()
-            this.videoContainer.style.display = "block"
-        }
+        this.incomingRing.play();
+        // const starTimer = Date.now()/1000
+        setTimeout(async () => {
+            const confirmResult = window.confirm("Incomming call from "+ data.roomId)
+            if(confirmResult) {
+                this.incomingRing.pause()
+                if(!this.cameraAllowed || !this.micAllowed) {
+                    return alert("You can't answer! Please allow camera and microphone permissions!")
+                }
+                // if(Date.now()/1000-starTimer)
+                const { roomId } = data
+                await this.initLocalStream()
+                this.roomId = roomId
+                this.peerConnection = new RTCPeerConnection(config)
+                this.addLocalTrack()
+                this.peerConnection.ontrack = this.initRemoteStream
+                this.peerConnection.onicecandidate = this.sendIceCandidate
+                this.peerConnection.oniceconnectionstatechange = this.onStateChange
+                await this.createOffer()
+                this.videoContainer.style.display = "block"
+            } else {
+                this.incomingRing.pause()
+            }
+        }, 10)
     }
 
     onWebRtcOffer = async(data) => { // Call Sender 
         console.log('Socket event callback: webrtc_offer')
-        
+        this.outgoingRing.pause()
         // if (!isRoomCreator) {
             this.peerConnection = new RTCPeerConnection(config)
             this.addLocalTrack(this.peerConnection)
@@ -176,13 +226,11 @@ class TestVideoCall {
     }
 
     onStateChange = (event, forceClosed=false) => {
-        // if(this.peerConnection) {
-        //     console.log("stateChangeddd : ", this.peerConnection.iceConnectionState)
-        // }
         if(forceClosed || (this.peerConnection && this.peerConnection.iceConnectionState==="disconnected")) {
             if(this.peerConnection)
                 this.peerConnection.close()
-            for(let t of this.localStream.getTracks()) t.stop()
+            if(this.localStream)
+                for(let t of this.localStream.getTracks()) t.stop()
             setTimeout(() => {
                 this.videoContainer.style.display = "none"
                 this.localVideo.srcObject = null
@@ -193,6 +241,8 @@ class TestVideoCall {
                 this.localStream = null
                 this.remoteStream = null 
                 alert("Disconnected!")
+                this.outgoingRing.pause()
+                this.incomingRing.pause()
             }, 100)
         }
     }
